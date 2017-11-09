25adab5015214a1c61acff2eb056e77874086620
kartikn27 <kanagre@syr.edu>
2017-11-09 18:37:14 -0500

    added contact page, route and controller method

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'home' => 'static_pages#home'
-  get 'help' => 'static_pages#help'
-  get 'about' => 'static_pages#about'
+  get 'home' => 'static_pages#home', :as => :home
+  get 'help' => 'static_pages#help', :as => :help
+  get 'about' => 'static_pages#about', :as => :about
+  get 'contact' => 'static_pages#contact', :as => :contact
 
   root 'static_pages#home'
 
