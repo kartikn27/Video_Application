aadc9e37ddf7bb45dafbad510e9ab27cf9834b4f
kartikn27 <kanagre@syr.edu>
2017-11-11 14:10:35 -0500

    added private method to make request using rest client

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def login
-    login_url = I18n.t('login_url')
-
-    authentication_payload = {I18n.t('authentication_payload.client_id') => I18n.t('client_id'),
-                               I18n.t('authentication_payload.client_secret') => I18n.t('client_secret'),
-                               I18n.t('authentication_payload.username') => params[:session][:email],
-                               I18n.t('authentication_payload.password') => params[:session][:password],
-                               I18n.t('authentication_payload.grant_type') => I18n.t('authentication_payload.password')}
-
-    response = RestClient.post login_url, authentication_payload
-
-    user_obj = JSON.parse(response.body)
+   response = make_login_request
+   user_obj = JSON.parse(response.body)
 
     if response.code.eql?(200)
       log_in params[:session][:email], user_obj['access_token']
@@ -23,6 +14,20 @@ class SessionsController < ApplicationController
       format.html { redirect_to '/login', danger: 'Failed to login' }
     end
 
+  end
+
+  private
+
+  def make_login_request
+    login_url = I18n.t('login_url')
+    authentication_payload = {I18n.t('authentication_payload.client_id') => I18n.t('client_id'),
+                              I18n.t('authentication_payload.client_secret') => I18n.t('client_secret'),
+                              I18n.t('authentication_payload.username') => params[:session][:email],
+                              I18n.t('authentication_payload.password') => params[:session][:password],
+                              I18n.t('authentication_payload.grant_type') => I18n.t('authentication_payload.password')}
+
+    response = RestClient.post login_url, authentication_payload
+    response
   end
 
 end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,9 +1,7 @@
 class VideosController < ApplicationController
 
   def index
-    videos_url = I18n.t('video_url') + '?app_key=' + I18n.t('app_key')
-    response = RestClient.get videos_url
-    response_obj = JSON.parse(response.body)
+    response_obj = make_get_videos_request
     @video_info_index = []
     response_obj['response'].each do |video|
       @video_info_index.push({'id' => video['_id'],
@@ -14,9 +12,7 @@ class VideosController < ApplicationController
   end
 
   def show
-    video_url = I18n.t('video_url') + '/' + params[:id] + '?' + 'app_key=' + I18n.t('app_key')
-    response = RestClient.get video_url
-    response_obj = JSON.parse(response.body)['response']
+    response_obj = make_get_video_request
     @video_info_show = response_obj
 
     @video_id = 'zype' + '_' + @video_info_show['_id']
@@ -25,6 +21,22 @@ class VideosController < ApplicationController
     if @video_info_show['subscription_required'].eql?(true) and current_user_token
       @video_src_sub = I18n.t('video_player.domain') + @video_info_show['_id'] + I18n.t('video_player.access_token') + current_user_token
     end
+  end
+
+  private
+
+  def make_get_videos_request
+    videos_url = I18n.t('video_url') + '?app_key=' + I18n.t('app_key')
+    response = RestClient.get videos_url
+    response_obj = JSON.parse(response.body)
+    response_obj
+  end
+
+  def make_get_video_request
+    video_url = I18n.t('video_url') + '/' + params[:id] + '?' + 'app_key=' + I18n.t('app_key')
+    response = RestClient.get video_url
+    response_obj = JSON.parse(response.body)['response']
+    response_obj
   end
 
 end
